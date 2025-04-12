4147c1d3ba8d4d79694ff7143895408d81ade1e0
adrainnaj <adrainnajohnson@gmail.com>
2025-04-11 20:44:48 -0400

    Complete

--- Week1FinancialCalculator/src/main/java/org/example/Main.java
+++ Week1FinancialCalculator/src/main/java/org/example/Main.java
@@ -1,7 +1,33 @@
 package org.example;
 
+import java.util.Scanner;
+//M = P[r(1+r)^n] / [(1+r)^n-1]
+//r = interest rate
+//p = principle
+//n = loan length
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Enter your Principle");
+        Scanner scanner = new Scanner(System.in);
+        double principle = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.println("Enter your Interest Rate");
+        double interestRate = scanner.nextDouble();
+        scanner.nextLine();
+        System.out.println("Enter your Loan Years");
+        double loanLength = scanner.nextDouble();
+        double month = loanLength * 12;
+        scanner.nextLine();
+        double monthlyInterest = (interestRate/12)/100;
+
+            // 1+r
+        double v = 1 + monthlyInterest;
+        double n = Math.pow(v,month);
+        double left = principle*(monthlyInterest*n);
+        double right = n - 1;
+        double monthlyPayment = left/right;
+
+        double totalInterest = (monthlyPayment*month) - principle;
+        System.out.printf("Your monthly rate due is $%.2f and your monthly payment is $%.2f ", totalInterest, monthlyPayment);
+        }
     }
-}
